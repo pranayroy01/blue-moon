d4a813c537801a20df2a8156976197c8a08ca18a
pranayroy01 <pranayroy01@gmail.com>
2014-01-29 15:56:59 +0800

    learning haskell

---
+++ game.hs
@@ -0,0 +1,14 @@
+data Sent = Sent NP VP deriving Show
+data DET = The | Every | Some | No | Most deriving Show
+data CN = Girl | Boy | Princess | Dwarf | Giant | Wizard | Sword | Dagger deriving Show
+data ADJ = Fake deriving Show
+data RCN = RCN1 CN That VP | RCN2 CN That NP TV| RCN3 ADJ CN deriving Show
+data That = That deriving Show
+data VP = Laughed | Cheered | Shuddered | VP1 TV NP | VP2 DV NP NP| VP3 AV To INF deriving Show
+data TV = Loved | Admired | Helped| Defeated | Caught deriving Show
+data DV = Gave deriving Show
+data AV = Hoped | Wanted deriving Show
+data NP = SnowWhite | Alice | Dorothy | Goldilocks| LittleMook | Atreyu | Everyone | Someone| NP1 DET CN | NP2 DET RCN deriving Show
+data INF = Laugh | Cheer | Shudder | INF TINF NP deriving Show
+data TINF = Love | Admire | Help | Defeat | Catch deriving Show
+data To = To deriving Show
